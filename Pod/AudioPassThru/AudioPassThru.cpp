f00a376543ab9693ba9dcc1589c55cf194b24128
percyc409 <78789641+percyc409@users.noreply.github.com>
2023-04-13 19:10:52 +0100

    Audio Pass Thru created with correct procedure this time. Notes on using helper.py added

--- Pod/AudioPassThru/AudioPassThru.cpp
+++ Pod/AudioPassThru/AudioPassThru.cpp
@@ -1,61 +1,27 @@
-#include "daisysp.h"
 #include "daisy_pod.h"
+#include "daisysp.h"
 
-using namespace daisysp;
 using namespace daisy;
+using namespace daisysp;
 
-static DaisyPod pod;
+DaisyPod hw;
 
-void AudioCallback(AudioHandle::InterleavingInputBuffer  in,
-                   AudioHandle::InterleavingOutputBuffer out,
-                   size_t                                size)
+void AudioCallback(AudioHandle::InputBuffer in, AudioHandle::OutputBuffer out, size_t size)
 {
-
-    //audio
-    for(size_t i = 0; i < size; i += 2)
-    {
-        // left out
-        out[i] = in[i];
-        // right out
-        out[i + 1] = in[i + 1];
-
-    }
+	hw.ProcessAllControls();
+	for (size_t i = 0; i < size; i++)
+	{
+		out[0][i] = in[0][i];
+		out[1][i] = in[1][i];
+	}
 }
 
 int main(void)
 {
-    // initialize pod hardware and oscillator daisysp module
-    float sample_rate;
-
-    //Inits and sample rate
-    pod.Init();
-    pod.SetAudioBlockSize(4);
-    sample_rate = pod.AudioSampleRate();
-
-    /*
-    rev.Init(sample_rate);
-    dell.Init();
-    delr.Init();
-    tone.Init(sample_rate);
-
-    //set parameters
-    deltime.Init(pod.knob1, sample_rate * .05, MAX_DELAY, deltime.LOGARITHMIC);
-    cutoffParam.Init(pod.knob1, 500, 20000, cutoffParam.LOGARITHMIC);
-    crushrate.Init(pod.knob2, 1, 50, crushrate.LOGARITHMIC);
-
-    //reverb parameters
-    rev.SetLpFreq(18000.0f);
-    rev.SetFeedback(0.85f);
-
-    //delay parameters
-    currentDelay = delayTarget = sample_rate * 0.75f;
-    dell.SetDelay(currentDelay);
-    delr.SetDelay(currentDelay);
-    */
-   
-    // start callback
-    pod.StartAdc();
-    pod.StartAudio(AudioCallback);
-
-    while(1) {}
+	hw.Init();
+	hw.SetAudioBlockSize(4); // number of samples handled per callback
+	hw.SetAudioSampleRate(SaiHandle::Config::SampleRate::SAI_48KHZ);
+	hw.StartAdc();
+	hw.StartAudio(AudioCallback);
+	while(1) {}
 }
