31420438ec7b319479930a7f1056b3d9ca71a0f7
percyc409 <78789641+percyc409@users.noreply.github.com>
2023-12-20 23:05:21 +0000

    Replaced simple oversampling with cmsis interpolation/decimation with factor of 4

--- Terrarium/Non_Lin_Algs/Non_Lin_Algs.cpp
+++ Terrarium/Non_Lin_Algs/Non_Lin_Algs.cpp
@@ -1,20 +1,32 @@
 #include "daisy_petal.h"
 #include "daisysp.h"
 #include "terrarium.h"
-#include "Oversampling.h"
 #include "highshelf.h"
+#include "arm_math.h"
+#include "polyphaseFIRcoefs.h"
 
 
 using namespace daisy;
 using namespace daisysp;
 using namespace terrarium;
 
+#define NUM_TAPS 96
+#define BLOCK_SIZE 4
+#define L 4 // Also M
+
 DaisyPetal hw;
 dsy_gpio led1;
 dsy_gpio led2;
 
-Oversampling<4,1> Oversampling_o;
+//Oversampling<4,1> Oversampling_o;
 Highshelf Pre_emphasis, Post_filt;
+
+arm_fir_interpolate_instance_f32 upsampler_i;
+arm_fir_decimate_instance_f32 downsampler_i;
+
+static float upsamp_state[(NUM_TAPS/L)+BLOCK_SIZE-1];
+static float downsamp_state[NUM_TAPS+(BLOCK_SIZE*L)-1];
+static float ovrsmp_arr[L * BLOCK_SIZE];
 
 Overdrive overdrive;
 
@@ -26,6 +38,7 @@ Parameter drive;
 int nl_alg;
 bool bypass = true;
 bool dsy_drive;
+
 
 void ProcessControls() {
 	
@@ -111,24 +124,36 @@ void AudioCallback(AudioHandle::InputBuffer in, AudioHandle::OutputBuffer out, s
 			out[0][i] = in[0][i];
 	}
 	else {
+	
+		float mono_in[BLOCK_SIZE] = {};
+		float mono_out[BLOCK_SIZE] = {};
+		
+		for (int i = 0; i<BLOCK_SIZE; i++)
+			mono_in[i] = in[0][i];
 
-		Oversampling_o.Upsample(in, out, size);
-		auto ovrsmp_arr = Oversampling_o.getArray();
 
-		for (int i = 0; i < 8; i++) {
+		//Upsample
+		arm_fir_interpolate_f32(&upsampler_i, mono_in, ovrsmp_arr, BLOCK_SIZE);
 
-			ovrsmp_arr[0][i] = Pre_emphasis.Process(ovrsmp_arr[0][i]);
+
+		for (int i = 0; i < L * BLOCK_SIZE; i++) {
+			
+			ovrsmp_arr[i] = Pre_emphasis.Process(ovrsmp_arr[i]);
 
 			if(dsy_drive)
-				ovrsmp_arr[0][i] = overdrive.Process(ovrsmp_arr[0][i]) * driveLevel.Value();
+				ovrsmp_arr[i] = overdrive.Process(ovrsmp_arr[i]) * driveLevel.Value();
 			else
-				ovrsmp_arr[0][i] = NonLinearProcess(ovrsmp_arr[0][i]);
+				ovrsmp_arr[i] = NonLinearProcess(ovrsmp_arr[i]);
 
-			ovrsmp_arr[0][i] = Post_filt.Process(ovrsmp_arr[0][i]);
+			ovrsmp_arr[i] = Post_filt.Process(ovrsmp_arr[i]);
 
 		}
 
-		Oversampling_o.Downsample(in, out, size);
+		//Downsample
+		arm_fir_decimate_f32(&downsampler_i, ovrsmp_arr, mono_out, BLOCK_SIZE*L);
+
+		for (int i = 0; i<BLOCK_SIZE; i++)
+			out[0][i] = mono_out[i];
 
 	}	
 }
@@ -139,9 +164,9 @@ int main(void)
 	hw.SetAudioBlockSize(4); // number of samples handled per callback
 	hw.SetAudioSampleRate(SaiHandle::Config::SampleRate::SAI_48KHZ);
 
-	Oversampling_o.Init();
-	Pre_emphasis.Init(hw.AudioSampleRate() * 2);
-	Post_filt.Init(hw.AudioSampleRate() * 2);
+	//Oversampling_o.Init();
+	Pre_emphasis.Init(hw.AudioSampleRate() * L);
+	Post_filt.Init(hw.AudioSampleRate() * L);
 	Pre_emphasis.SetFreq(8000.0f);
 	Post_filt.SetFreq(8000.0f);
 	Pre_emphasis.SetGain(36.0F);
@@ -152,6 +177,9 @@ int main(void)
 	volume.Init(hw.knob[Terrarium::KNOB_2], 0.0f, 1.5f, Parameter::EXPONENTIAL);
 	driveLevel.Init(hw.knob[Terrarium::KNOB_5], 0.00f, 1.0f, Parameter::EXPONENTIAL);
     drive.Init(hw.knob[Terrarium::KNOB_4], 0.0f, 0.8f, Parameter::LINEAR);
+
+	arm_fir_interpolate_init_f32(&upsampler_i, L, NUM_TAPS, (float *)&polyphaseFIRcoefs_4_reversed[0], &upsamp_state[0],BLOCK_SIZE);
+	arm_fir_decimate_init_f32(&downsampler_i, NUM_TAPS, L, (float *)&polyphaseFIRcoefs_4_reversed[0], &downsamp_state[0],BLOCK_SIZE*L);
 
 	hw.StartAdc();
 	hw.StartAudio(AudioCallback);
@@ -167,5 +195,8 @@ int main(void)
     led2.pull = DSY_GPIO_NOPULL;
     dsy_gpio_init(&led2);
 
-	while(1) {}
+	
+	while(1) {
+
+	}
 }
